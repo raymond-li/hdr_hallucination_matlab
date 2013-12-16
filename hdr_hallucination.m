736d1a83a3730cfd48d1fa98964be274f5bf9982
yhnsun <yhnsun@gmail.com>
2013-12-16 15:22:17 -0600

    Update hdr_hallucination.m

--- hdr_hallucination.m
+++ hdr_hallucination.m
@@ -11,5 +11,7 @@ function [ hallucinated ] = hdr_hallucination( input_image, varargin )
 % varargin{1} = overexposed mask
 % varargin{2} = underexposed mask
 
+im=gamma22(im2double(imread(input_image)));
+
 end
 
