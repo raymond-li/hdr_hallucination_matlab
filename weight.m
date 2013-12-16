ede9b1618623e60dfd7608375a938d35ca726470
yhnsun <yhnsun@gmail.com>
2013-12-16 15:46:18 -0600

    Update weight.m

--- weight.m
+++ weight.m
@@ -1,9 +1,9 @@
-function w=weight(Y, Cue, Coe)
+function [W]=weight(Y, Cue, Coe)
 %weight function
 if (Y < Cue)
-w = (Cue-Y)/Cue;
+W = (Cue-Y)/Cue;
 
 if (Coe < Y)
-w = (Y-Coe)/(1-Coe)
+W = (Y-Coe)/(1-Coe)
 
 end
